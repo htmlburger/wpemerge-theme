2913b234716fc8ebba8d6aaf64d03073e4d2896c
atanas-angelov-dev <atanas.angelov.dev@gmail.com>
2018-05-28 23:57:17 +0300

    remove unnecessary php tags in templates

--- theme/partials/post-meta.php
+++ theme/partials/post-meta.php
@@ -14,18 +14,19 @@ if ( empty( $post ) || get_post_type() !== 'post' ) {
 
 <div class="article-meta">
 	<p>
-		<?php the_time( 'F jS, Y ' ); ?>
 		<?php
+		the_time( 'F jS, Y ' );
 		/* translators: post author attribution */
 		printf( __( 'by %s', 'app' ), get_the_author() );
 		?>
 	</p>
 
 	<p>
-		<?php _e( 'Posted in ', 'app' ); ?>
-		<?php the_category( ', ' ); ?> |
 		<?php
+		_e( 'Posted in ', 'app' );
+		the_category( ', ' );
 		if ( comments_open() ) {
+			echo ' | ';
 			comments_popup_link( __( 'No Comments', 'app' ), __( '1 Comment', 'app' ), __( '% Comments', 'app' ) );
 		}
 		?>
