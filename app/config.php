4973d1d8ff4ee614025252bc36df1ee89f82241d
Atanas Angelov <atanas.angelov.dev@gmail.com>
2019-01-25 02:34:06 +0200

    Add the new views config option to config.php

--- app/config.php
+++ app/config.php
@@ -23,6 +23,13 @@ return [
 	],
 
 	/**
+	 * Custom directory to search for views.
+	 * Use an absolute path or leave blank to disable.
+	 * Applies only to the default PhpViewEngine.
+	 */
+	'views' => '',
+
+	/**
 	 * Other config goes after this comment.
 	 */
 
