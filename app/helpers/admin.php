850a57ae82091435448c7d012f2caddb6d2fc361
Atanas Angelov <atanas.angelov.dev@gmail.com>
2019-04-20 21:56:18 +0300

    Add admin.php helper

---
+++ app/helpers/admin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Admin helpers which register admin pages.
+ *
+ * @link https://developer.wordpress.org/reference/functions/add_menu_page/
+ * @link https://developer.wordpress.org/reference/functions/add_submenu_page/
+ *
+ * @package WPEmergeTheme
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
