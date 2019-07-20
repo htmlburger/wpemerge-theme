5637f7f03c67a7367d2505460f1716a334d98e31
Atanas Angelov <hi@atanas.dev>
2019-07-21 00:08:27 +0300

    Add image lazyloading (disabled by default)

--- resources/scripts/theme/index.js
+++ resources/scripts/theme/index.js
@@ -4,5 +4,7 @@ import './vendor/*.js';
 import '@styles/theme';
 import '@images/favicon.ico';
 import 'airbnb-browser-shims';
+// Uncomment the following line to enable lazy-loading of media images:
+// import './lazyload';
 
 // Your code goes here ...
---
+++ resources/scripts/theme/lazyload.js
@@ -0,0 +1,5 @@
+import 'intersection-observer';
+import lozad from 'lozad';
+
+const observer = lozad();
+observer.observe();
