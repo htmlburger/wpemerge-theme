706aea5c027756bd5e590aa0f0a7e4ba5dab223c
Atanas Angelov <hi@atanas.dev>
2019-10-10 20:35:27 +0300

    Handle images and fonts referenced in node_modules

--- resources/build/lib/utils.js
+++ resources/build/lib/utils.js
@@ -52,8 +52,8 @@ module.exports.distFontsPath = destPath =>
 module.exports.tests = {
   scripts: /\.(js|jsx)$/,
   styles: /\.(css|scss|sass)$/,
-  images: /images[\\/].*\.(ico|jpg|jpeg|png|svg|gif)$/,
-  fonts: /fonts[\\/].*\.(eot|svg|ttf|woff|woff2)$/,
+  images: /(resources[\\/]images|node_modules)[\\/].*\.(ico|jpg|jpeg|png|svg|gif)$/,
+  fonts: /(resources[\\/]fonts|node_modules)[\\/].*\.(eot|svg|ttf|woff|woff2)$/,
 };
 
 module.exports.detectEnv = () => {
