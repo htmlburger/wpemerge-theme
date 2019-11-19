1cd25cfb86f30166c0bebb2be41ceafb8c2cee8b
Atanas Angelov <hi@atanas.dev>
2019-11-19 23:52:08 +0200

    Add generic error views for admin and ajax requests

---
+++ views-alternatives/blade/error-ajax.blade.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Generic error fallback view for ajax requests.
+ * Used if no view is found for the current error status code.
+ *
+ * @package WPEmergeTheme
+ */
+
+?>
+<p>
+	{!!
+	sprintf(
+		/* translators: generic error page content; placeholders represents homepage opening and closing anchor tags */
+		esc_html__( 'Please check the URL for proper spelling and capitalization. If you\'re having trouble locating a destination, try visiting the %1$shome page%2$s.', 'app' ),
+		'<a href="' . esc_url( home_url( '/' ) ) . '">',
+		'</a>'
+	)
+	!!}
+</p>
