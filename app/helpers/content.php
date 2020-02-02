8fb4b46c80d76ab1a2c00f2c2140053abc9c8ce9
Atanas Angelov <hi@atanas.dev>
2020-02-03 01:00:21 +0200

    Improve content functions

--- app/helpers/content.php
+++ app/helpers/content.php
@@ -69,11 +69,16 @@ function app_filter_fix_upload_dir_url_schema( $upload_dir ) {
 /**
  * Get text suitable for the title attribute of a permalink anchor tag.
  *
+ * @param integer $post_id Post ID to get the title of. Defaults to the current post.
  * @return string The title attribute value
  */
-function app_get_permalink_title() {
+function app_get_permalink_title( $post_id = 0 ) {
+	if ( 0 === $post_id ) {
+		$post_id = get_the_ID();
+	}
+
 	/* translators: post link title attribute */
-	return sprintf( __( 'Permanent Link to %s', 'app' ), get_the_title() );
+	return sprintf( __( 'Permanent Link to %s', 'app' ), get_the_title( $post_id ) );
 }
 
 /**
@@ -89,24 +94,28 @@ function app_content( $content ) {
 }
 
 /**
- * Get text suitable for the title attribute of a permalink anchor tag.
+ * Get the 404 page content.
  *
  * @return string The title attribute value
  */
 function app_get_index_404_message() {
 	if ( is_category() ) {
 		/* translators: no posts found for category */
-		return sprintf( __( 'Sorry, but there aren\'t any posts in the %s category yet.', 'app' ), single_cat_title( '', false ) );
+		return sprintf( __( 'There are no posts in the %s category yet.', 'app' ), single_cat_title( '', false ) );
+	}
+	if ( is_tag() ) {
+		/* translators: no posts found with tag */
+		return sprintf( __( 'There are no posts with the %s tag yet.', 'app' ), single_tag_title( '', false ) );
 	}
 
 	if ( is_date() ) {
-		return __( 'Sorry, but there aren\'t any posts with this date.', 'app' );
+		return __( 'There are no posts published on this date.', 'app' );
 	}
 
 	if ( is_author() ) {
-		$userdata = get_user_by( 'id', get_queried_object_id() );
-		/* translators: no posts found for author */
-		return sprintf( __( 'Sorry, but there aren\'t any posts by %s yet.', 'app' ), $userdata->display_name );
+		$user = get_user_by( 'id', get_queried_object_id() );
+		/* translators: no posts found by author */
+		return sprintf( __( 'There are no posts published by %s yet.', 'app' ), $user->display_name );
 	}
 
 	if ( is_search() ) {
