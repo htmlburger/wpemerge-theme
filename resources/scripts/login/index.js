530c99eaa6141408d6e49f8271f97f2b59c31024
Atanas Angelov <hi@atanas.dev>
2020-11-24 03:28:44 +0200

    Remove airbnb-browser-shims as it is overkill with modern browsers

--- resources/scripts/login/index.js
+++ resources/scripts/login/index.js
@@ -1,7 +1,5 @@
 // eslint-disable-next-line no-unused-vars
 import config from '@config';
 import '@styles/login';
-// Uncomment the following line if needed:
-// import 'airbnb-browser-shims';
 
 // Your code goes here ...
